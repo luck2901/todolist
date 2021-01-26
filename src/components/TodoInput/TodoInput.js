eb6909d470f7cf65042770754e08c62180953d30
unknown <luck2901@naver.com>
2021-01-26 19:40:19 +0900

    Update

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -5,12 +5,6 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-// 인풋과 버튼이 함께 있는 컴포넌트입니다. 
-/*
-  value: 인풋 값
-  onChange: 인풋 변경 이벤트
-  onInsert: 추가버튼 클릭 이벤트
-*/
 const TodoInput = ({value, onChange, onInsert}) => {
   
   // 엔터키가 눌리면 onInsert 를 실행합니다.
